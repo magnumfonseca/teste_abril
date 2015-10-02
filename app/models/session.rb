c01d98156004dc7016699c9e36e698a2e57875c3
magnumfonseca <magnum.td.fonseca@gmail.com>
2015-10-01 23:30:24 -0300

    fixed tasks without duration

--- app/models/session.rb
+++ app/models/session.rb
@@ -10,15 +10,27 @@ class Session
   def add(task)
     fail 'task is too long for session' unless add? task
     @available_time -= task.duration
-    @tasks << task
+    if @tasks.last && @tasks.last.duration == 0
+      @tasks.insert(@tasks.length - 1,  task)
+    else
+      @tasks << task
+    end
+
   end
 
   def add?(task)
-    return false if task.duration > @available_time
+    return false if task.duration > @available_time || has_unknow_duration_task?
     true
   end
 
   def include?(task)
     @tasks.include? task
+  end
+
+  def has_unknow_duration_task?
+    @tasks.each do |task|
+      return true if task.duration == 0
+    end
+    false
   end
 end
